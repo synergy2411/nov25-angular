48f0dace8e53528fe6ddbaff6c6197639b1e31f9
Sumit Khandelwal <synergy2411@outlook.com>
2025-11-04 10:37:50 +0530

    async and await

--- js-basics/async.js
+++ js-basics/async.js
@@ -16,9 +16,9 @@
 // // PROMISES
 
 // Promise Builder
-function buildPromise() {
+function buildPromise(ms) {
   let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve({ message: "success" }), 1000);
+    setTimeout(() => resolve({ message: "success" }), ms);
     // reject(new Error("Something went wrong!"));
   });
   return promise;
@@ -28,19 +28,34 @@ function buildPromise() {
 // - then().catch()
 // - Async...await
 
-function consumePromise() {
-  console.log("Program Started");
-  buildPromise()
-    .then((response) => {
-      console.log("FIRST THEN :", response);
-      return response.message;
-    })
-    .then((message) => {
-      console.log("SECOND THEN :", message.toUpperCase());
-    })
-    .catch((err) => console.error(err));
-
-  console.log("Program Ended");
+async function consumePromiseUsingAsyncAndAwait() {
+  try {
+    const responseOne = await buildPromise(2000);
+    console.log("RESPONSE ONE: ", responseOne);
+    const responseTwo = await buildPromise(1000);
+    console.log("RESPONSE Two: ", responseTwo);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-consumePromise();
+console.log("Start");
+consumePromiseUsingAsyncAndAwait();
+console.log("End");
+
+// function consumePromiseUsingThenAndCatch() {
+//   console.log("Program Started");
+//   buildPromise()
+//     .then((response) => {
+//       console.log("FIRST THEN :", response);
+//       return response.message;
+//     })
+//     .then((message) => {
+//       console.log("SECOND THEN :", message.toUpperCase());
+//     })
+//     .catch((err) => console.error(err));
+
+//   console.log("Program Ended");
+// }
+
+// consumePromiseUsingThenAndCatch();
