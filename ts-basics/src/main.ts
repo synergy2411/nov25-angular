cafc888be94a444b2e60c3adac8ae94dde0b2c4a
Sumit Khandelwal <synergy2411@outlook.com>
2025-11-04 13:06:37 +0530

    classes added

--- ts-basics/src/main.ts
+++ ts-basics/src/main.ts
@@ -45,61 +45,150 @@
 
 // // VARIOUS TYPES IN TYPESCRIPT
 
-let strVar: string;
-strVar = "Hola";
+// let strVar: string;
+// strVar = "Hola";
 
-let numVar: number;
-numVar = 199;
+// let numVar: number;
+// numVar = 199;
 
-let boolVar: boolean;
-boolVar = true;
+// let boolVar: boolean;
+// boolVar = true;
 
-let dateVar: Date;
-dateVar = new Date("November 04, 2025");
-console.log("Date : ", dateVar);
+// let dateVar: Date;
+// dateVar = new Date("November 04, 2025");
+// console.log("Date : ", dateVar);
 
-let strArrayVar: Array<string>;
-strArrayVar = ["Monica", "Joey", "Ross"];
+// let strArrayVar: Array<string>;
+// strArrayVar = ["Monica", "Joey", "Ross"];
 
-let numArrayVar: number[];
-numArrayVar = [98, 97, 92, 89];
+// let numArrayVar: number[];
+// numArrayVar = [98, 97, 92, 89];
 
-let anyVar: any;
-anyVar = "The String";
-anyVar = 100;
-anyVar = true;
+// let anyVar: any;
+// anyVar = "The String";
+// anyVar = 100;
+// anyVar = true;
 
-let unknownVar: unknown;
-unknownVar = "The String";
-if (typeof unknownVar == "string") {
-  console.log("Index of 'S': ", unknownVar.indexOf("S"));
+// let unknownVar: unknown;
+// unknownVar = "The String";
+// if (typeof unknownVar == "string") {
+//   console.log("Index of 'S': ", unknownVar.indexOf("S"));
+// }
+
+// unknownVar = 199;
+// unknownVar = true;
+
+// // Fixed length and varying types array sub-set
+// let tupleVar: [string, number];
+// tupleVar = ["John Doe", 21];
+
+// let unionVar: string | number;
+// unionVar = "Twenty-One";
+// unionVar = 21;
+
+// let functioVar: Function;
+// functioVar = () => {};
+
+// let functionVar2: () => string;
+// functionVar2 = () => "Hello World";
+
+// let nullOrStringVar: null | string;
+// nullOrStringVar = null;
+// nullOrStringVar = "The String";
+
+// function demoVoid(): void {
+//   console.log("Void Function");
+// }
+
+// function throwError(): never {
+//   throw new Error("Something went wrong");
+// }
+
+// // CLASESS
+
+// class Person {
+//   // private name: string;
+//   // private age: number;
+
+//   constructor(private name: string, private age: number) {
+//     // this.name = name;
+//     // this.age = age;
+//   }
+
+//   getDetails(): string {
+//     return `${this.name} - ${this.age}`;
+//   }
+// }
+
+// let personOne = new Person("Monica Geller", 21);
+// console.log(personOne.getDetails());
+
+// class Student extends Person {
+//   static numOfStudentsEnrolled: number = 0;
+
+//   constructor(
+//     private studId: string,
+//     private course: string,
+//     name: string,
+//     age: number
+//   ) {
+//     super(name, age);
+//     Student.numOfStudentsEnrolled += 1;
+//   }
+
+//   getDetails(): string {
+//     return `Student Details - ${this.studId} - ${
+//       this.course
+//     } | ${super.getDetails()} `;
+//   }
+// }
+
+// let studentOne = new Student(
+//   "S001",
+//   "Angular Intermediate",
+//   "Rachel Green",
+//   22
+// );
+
+// console.log(studentOne.getDetails());
+// let studentTwo = new Student(
+//   "S002",
+//   "Angular Intermediate",
+//   "Chandler Bing",
+//   24
+// );
+
+// console.log(studentTwo.getDetails());
+
+// console.log("Total Enrollments : ", Student.numOfStudentsEnrolled);
+
+// // ABSTRACT CLASSES
+
+abstract class Recipe {
+  abstract collectIngredients(): void;
+  abstract prepareTheDish(): void;
+  abstract cleanUp(): void;
+
+  execute() {
+    this.collectIngredients();
+    this.prepareTheDish();
+    this.cleanUp();
+  }
 }
 
-unknownVar = 199;
-unknownVar = true;
-
-// Fixed length and varying types array sub-set
-let tupleVar: [string, number];
-tupleVar = ["John Doe", 21];
-
-let unionVar: string | number;
-unionVar = "Twenty-One";
-unionVar = 21;
-
-let functioVar: Function;
-functioVar = () => {};
-
-let functionVar2: () => string;
-functionVar2 = () => "Hello World";
-
-let nullOrStringVar: null | string;
-nullOrStringVar = null;
-nullOrStringVar = "The String";
-
-function demoVoid(): void {
-  console.log("Void Function");
+class Tea extends Recipe {
+  collectIngredients(): void {
+    console.log("Ready with Tea leafs, suger, water and milk");
+  }
+  prepareTheDish(): void {
+    console.log("Boil Water");
+    console.log("Add the ingredients");
+    console.log("Serve the Tea");
+  }
+  cleanUp(): void {
+    console.log("Clean all utensils");
+  }
 }
 
-function throwError(): never {
-  throw new Error("Something went wrong");
-}
+let gingerTea = new Tea();
+gingerTea.execute();
