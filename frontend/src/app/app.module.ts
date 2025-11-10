5061b0e88fd30dbaf290e3c55272457e70aa3bb7
Sumit Khandelwal <synergy2411@outlook.com>
2025-11-10 10:12:31 +0530

    model form added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
@@ -39,6 +39,7 @@ import { RegisterComponent } from './components/auth/register/register.component
     // Imports module functionality
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [], // Creates tokens and register the Services
   bootstrap: [AppComponent],
--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: new FormControl(),
+      password: new FormControl(),
+    });
+  }
+
+  onSubmit() {
+    const { username, password } = this.registerForm.value;
+    console.log('USername : ', username);
+    console.log('Password : ', password);
+  }
 }
